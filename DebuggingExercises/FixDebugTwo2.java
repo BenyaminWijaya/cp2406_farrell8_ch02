e9ada8bc51b9c70327fefa8796c6115c3b1cf63a
Benyamin Wijaya <Theresearcher132>
2016-03-22 17:50:08 +0800

    Rejected commit: Default

---
+++ DebuggingExercises/FixDebugTwo2.java
@@ -0,0 +1,16 @@
+public class DebugTwo2
+//  This application performs arithmetic with two integers
+{
+   public static void main(String[] args)
+   {
+      int a, b;
+      a = 7;
+      b = 4;
+      sum = a + b;
+      difference = a - b;
+      product = a * b;
+      System.out.println("The sum is " + sum);
+      System.out.println("The difference is " + difference);
+      System.out.println("The product is " + product);
+   }
+}
