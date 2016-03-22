823a0764efe5a00ff5f9768058eb2f6f3d0b6ba4
Benyamin Wijaya <Theresearcher132>
2016-03-22 18:29:40 +0800

    Rejected commit: Default

--- DebuggingExercises/FixDebugTwo4.java
+++ DebuggingExercises/FixDebugTwo4.java
@@ -1,15 +1,16 @@
 import javax.swing.JOptionPane;
 public class DebugTwo4
 {
-   public static void main(String[] args)
-   {
+      public static void main(String[] args) {
       String costString;
       double cost;
       final double TAX = 0.06;
-      costString = JOptionPane.showInputDialog("Enter price of item you are buying", "Purchases",
-         JOptionPane.INFORMATION_MESSAGE);
-      cost = double.parseInt(costString);
-      JOptionPane.showMessageDialog(null,"With " + TAX * 100 +
-         "% tax,  purchase  is $" + (cost - cost * TAX));
+      JFrame frame = new JFrame("input");
+      costString = JOptionPane.showInputDialog(frame,"Enter price of item you are buying", "Purchases",
+              JOptionPane.INFORMATION_MESSAGE);
+      cost = Double.parseDouble(costString);
+      JOptionPane.showMessageDialog(null, "With " + TAX * 100 +
+              "% tax,  purchase  is $" + (cost - cost * TAX));
    }
+
 }
